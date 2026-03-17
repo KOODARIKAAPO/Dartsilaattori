f9bfb8fa967bb7524e2fd3980791b7b07d002279
TiitusPinola <tiitus.pinola@gmail.com>
2026-03-17 10:07:53 +0200

    teema

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { Provider as PaperProvider } from "react-native-paper";
 import Navigation from "./app/Navigation";
+import { theme } from "./ui/Theme";
 
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <Navigation />
     </PaperProvider>
   );
