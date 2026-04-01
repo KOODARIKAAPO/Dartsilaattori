b458226495ed93736b3c847374779b2755f41327
TiitusPinola <tiitus.pinola@gmail.com>
2026-04-01 10:18:41 +0300

    commit

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { User } from "firebase/auth";
 
 import { RootStackParamList } from "../types/NavigationType";
